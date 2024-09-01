6f94a04e78378436418cbc186bb2a3bbcb8dc19a
Atharv Lingayat <atharvofficial7@gmail.com>
2024-09-01 19:42:47 +0530

    CORS configuration and env setup

--- server/index.js
+++ server/index.js
@@ -5,10 +5,30 @@ import bodyParser from "body-parser";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import gql from "graphql-tag";
+
 const app = express();
+
+// Allow requests from multiple origins
+const allowedOrigins = [
+  'https://graphql-posts-app.vercel.app',
+  'http://localhost:5173'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    // If the request is from one of the allowed origins, allow it
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
+app.use(bodyParser.json({ limit: "16kb" }));
+
 const server = new ApolloServer({
   typeDefs: gql`
-
     type Company{
       name: String
     },
@@ -42,33 +62,28 @@ const server = new ApolloServer({
     }
   `,
   resolvers: {
-
-    Post:{
-        user: async(parent) =>{
-            return (await axios.get(`https://jsonplaceholder.typicode.com/users/${parent.userId}`))
-        .data;
-        }
+    Post: {
+      user: async (parent) => {
+        return (await axios.get(`https://jsonplaceholder.typicode.com/users/${parent.userId}`))
+          .data;
+      }
     },
     Query: {
       getPosts: async () => {
         return (await axios.get("https://jsonplaceholder.typicode.com/posts"))
           .data;
       },
-
-      getComments: async (_ , {postId})=>{
+      getComments: async (_, { postId }) => {
         const allComments = (await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)).data
         return allComments
       },
-      
-      getUserDetails: async (_, {userId})=>{
+      getUserDetails: async (_, { userId }) => {
         return (await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)).data
       }
     },
   },
 });
 
-app.use(cors());
-app.use(bodyParser.json({ limit: "16kb" }));
 await server.start();
 app.use("/graphql", expressMiddleware(server));
 
